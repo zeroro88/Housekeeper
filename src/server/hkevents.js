348c3d82d8538b5ebba8c4fb4658fd5abcb37303
Alan Liu <aliu@emergentpayments.net>
2016-03-27 11:19:27 +0800

    add /events/search

--- src/server/hkevents.js
+++ src/server/hkevents.js
@@ -7,7 +7,6 @@ let router = express.Router();
 router.get('/events', function(req, res, next) {
   let db = dataAccessAdapter.GetDB();
 
-  let result = [];
   db.all("SELECT rowid AS id, name, type, logdata FROM hkevents", function(err, rows) {
     let result = [];
     _.each(rows, function(row, key, list) {
@@ -21,7 +20,20 @@ router.get('/events', function(req, res, next) {
     res.json(result);
   });
 });
-
+router.get('/events/search', function(req, res, next) {
+  dataAccessAdapter.GetDB().all("SELECT rowid AS id, name, type, logdata FROM hkevents where name = ?",[req.query.name], function(err, rows) {
+    let result = [];
+    _.each(rows, function(row, key, list) {
+      result.push({
+        "id": row.id,
+        "name": row.name,
+        "type": row.type,
+        "logdata": row.logdata
+      });
+    });
+    res.json(result);
+  });
+});
 router.post('/events', function(req, res, next) {
   dataAccessAdapter.GetDB().run("INSERT INTO hkevents VALUES (?,?,?)", [req.body.name, req.body.type, req.body.logdata], function(err, row) {
     if (err) {
